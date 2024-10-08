54625b85761c07a39fe4acbba22ba1b4999aea70
DevFawadKhan <mfawadkhan05@gmail.com>
2024-10-08 08:42:36 -0700

    login api complete

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,14 @@
 import express from "express";
-import {body, validationResult} from 'express-validator'
+import {body} from 'express-validator'
 import {signup} from "../Controller/signup.js";
+import {login} from "../Controller/login.js";
 const router=express.Router()
 
 router.get("/user",(req,res)=>{
     res.send("test api")
 })
-
 router.post('/signup', [body('name').isLength({min:3}), body("email").isEmail(),body('cnic').isLength({min:13, max:13}), body('contactNumber').isLength({min:11, max:11}), ], signup);
+router.post('/login',[body('email').isEmail()],login)
 
 
 export default router;
