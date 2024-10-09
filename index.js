c92e4b1e770f4486d92b2e4ac6a3588107396f8d
DevFawadKhan <mfawadkhan05@gmail.com>
2024-10-09 08:49:14 -0700

    Add Admin login and model

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // import express from "express"
 import dotenv from 'dotenv';
 import express from "express";
-
 import user from './routes/user.js';
+import admin from './routes/Admin.js'
 import  dbconnect  from './DB/db.js';
 const app = express();
 dotenv.config();
@@ -12,7 +12,6 @@ app.use(express.json());
 // Connect to MongoDB
 dbconnect(app);
 
-
 app.use('/api',user)
-
+app.use('/api/admin',admin)
 
