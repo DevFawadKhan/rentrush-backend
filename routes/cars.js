d2872124fba6a07276c8da66e59076f2444bbdfe
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2024-10-26 20:12:04 +0500

    multer error resolved

--- routes/cars.js
+++ routes/cars.js
@@ -2,7 +2,7 @@ import express from 'express'
  import multer from 'multer'
  import { removeCar,addCar,searchCar,updateCar,getAllCars } from '../Controller/carsController.js'
  import { verifyToken } from '../Middleware/verifyToken.js';
- const path="./uploads";
+ const path="../../RentRush/public/uploads";
  const storage=multer.diskStorage({
     destination: function(req,file,cb){ return cb(null,path);},
     filename: function(req,file,cb){return cb(null,`${Date.now()}-${file.originalname}`)}
@@ -10,7 +10,7 @@ import express from 'express'
  const upload=multer({storage})
  console.log(path)
 const router= express.Router()
-router.post('/add',upload.single('imgfile'), verifyToken,addCar)
+router.post('/add',upload.single('images'), verifyToken,addCar)
 router.put('/update:carId', verifyToken,updateCar)
 router.get('/get-all-cars', verifyToken,getAllCars)
 
