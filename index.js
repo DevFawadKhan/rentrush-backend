ed4a3598db3a5566154d3f0d5153c4a6a81ada5e
DevFawadKhan <mfawadkhan05@gmail.com>
2024-11-25 05:17:53 -0700

    working on return a car module

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // import express from "express"
 import dotenv from 'dotenv';
 import express from "express";
+import http from 'http'
+import {Server} from 'socket.io'
 import user from './routes/user.js';
 import Booking from './routes/bookingRoute.js'
 import admin from './routes/Admin.js'
@@ -9,15 +11,22 @@ import car from './routes/cars.js'
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 const app = express();
+const server=http.createServer(app);
 dotenv.config();
-
+export const io=new Server(server,{
+  cors:{
+    origin:"'http://localhost:5173'",
+    method:["GET","POST"]
+  }
+ })
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:5173', 
     credentials: true,
   }));
-dbconnect(app);
+  
+dbconnect(server);
 app.use('/api', user)
 app.use('/api/admin', admin)
 app.use('/api/car', car);
